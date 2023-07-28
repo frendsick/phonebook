a94986b5fd188d2662a1eaedb4fa896d3ee22a3e
Teemu Pätsi <teemu@paddy.fi>
2023-07-28 21:00:05 +0300

    API endpoint for displaying single phonebook entry

--- index.js
+++ index.js
@@ -28,14 +28,24 @@ let persons = [
     },
 ];
 
-app.get("/api/persons", (_, res) => {
-    res.json(persons);
-});
-
+// === VIEWS ===
 app.get("/info", (_, res) => {
     const personCount = persons.length;
     const timeNow = new Date();
     res.render("info", { personCount, timeNow });
+});
+
+// === API ===
+app.get("/api/persons", (_, res) => {
+    res.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+
+    if (person) response.json(person);
+    else response.status(404).end();
 });
 
 const PORT = 3001;
