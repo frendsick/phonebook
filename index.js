96e7129d0e4c9babd2c8544c47283e906c7965ce
Teemu Pätsi <teemu@paddy.fi>
2023-07-28 21:52:19 +0300

    Allow setting the port with PORT environment variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+require("dotenv").config();
 
 // Allow requests from other origins
 const cors = require("cors");
@@ -101,7 +102,7 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
