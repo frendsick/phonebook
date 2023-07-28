6cb96d9eee96413b5c21e480c6f0006fc0c43780
Teemu Pätsi <teemu@paddy.fi>
2023-07-28 21:07:01 +0300

    Implement delete API for persons

--- index.js
+++ index.js
@@ -48,6 +48,16 @@ app.get("/api/persons/:id", (request, response) => {
     else response.status(404).end();
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (!person) response.status(404).end(); // Person not found
+
+    // Delete the person
+    persons = persons.filter((person) => person.id !== id);
+    response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
