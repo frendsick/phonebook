f001b8f4fb3dd55eb3bc5fb4a29157b92dfce18f
Teemu Pätsi <teemu@paddy.fi>
2023-07-29 01:41:36 +0300

    Remove mock persons

--- backend/index.js
+++ backend/index.js
@@ -21,29 +21,6 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :b
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122",
-    },
-];
-
 // === VIEWS ===
 app.get("/info", async (_, response) => {
     const persons = await fetchPersons();
