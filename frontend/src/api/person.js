38a732f98e43e661ac93e9c88690cc53aa40872e
Teemu Pätsi <teemu@paddy.fi>
2023-07-28 22:44:08 +0300

    Fix API path constant

--- frontend/src/api/person.js
+++ frontend/src/api/person.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const PERSON_API = "/persons";
+const PERSON_API = "/api/persons";
 
 const getAll = () => axios.get(PERSON_API).then((response) => response.data);
 const create = (person) => axios.post(PERSON_API, person).then((response) => response.data);
