115381fa41ca00f8dbd6edc285855e02fd813bf4
CyberPaddy <teemu@paddy.fi>
2023-07-29 17:58:22 +0300

    Validate phone number format
    - Regular expression `\d{2,3}-\d+` ensures the format
    - `minLength` ensures that the number is long enough

--- backend/mongo.js
+++ backend/mongo.js
@@ -20,11 +20,18 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
-        required: true,
+        required: [true, "Name is required"],
     },
     number: {
         type: String,
-        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+        minLength: [9, "Not enough numbers"], // 8 numbers and the hyphen
+        required: [true, "Phone number is required"],
     },
 });
 
